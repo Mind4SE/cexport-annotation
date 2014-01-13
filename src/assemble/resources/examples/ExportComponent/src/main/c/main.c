72b68fad405e212f8f784f1fc2c13bae6a172a32
Julien TOUS <julien.tous@non.schneider-electric.com>
2014-01-13 16:51:33 +0100

    Add docbook documentation to the plugin

--- src/assemble/resources/examples/ExportComponent/src/main/c/main.c
+++ src/assemble/resources/examples/ExportComponent/src/main/c/main.c
@@ -1,11 +1,17 @@
-#include "clt_export.h"
+//Mind generated symbols declaration
+
+//Exported from the srv interface
 #include "srv_export.h"
+//required for the clt interface
+#include "clt_export.h"
 
 int main(void)
 {
+	//Using a symbol from "srv_export.h"
 	return srv_getNumber();
 }
 
+//Defining ALL symbols from "clt_export.h"
 uint8_t clt_getNumber(void)
 {
 	return 18;
